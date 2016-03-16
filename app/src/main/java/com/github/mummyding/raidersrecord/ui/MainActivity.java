4ddf604f20ade63c877118f967aea334e3e59ee1
MummyDing <mummydingz@gmail.com>
2016-03-16 19:33:58 +0800

    complete basic framework

--- app/src/main/java/com/github/mummyding/raidersrecord/ui/MainActivity.java
+++ app/src/main/java/com/github/mummyding/raidersrecord/ui/MainActivity.java
@@ -1,15 +1,46 @@
 package com.github.mummyding.raidersrecord.ui;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.github.mummyding.raidersrecord.R;
+import com.github.mummyding.raidersrecord.support.PagerAdapterWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("我的夺宝");
+        /**
+         * if this is a sub Activity ,you can add these code(it will display a back arrow)
+         */
+        /*getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });*/
+        PagerAdapterWrapper pagerAdapterWrapper = new PagerAdapterWrapper(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapterWrapper.addFragments(new LuckFragment(),new RaiderFragment()).build());
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
